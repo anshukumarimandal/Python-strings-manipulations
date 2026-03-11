f12bed64233cf02689d96f2cfbdbd762ad39b0d0
Anshukumari Mandal <anshukumarimandal09@gmail.com>
2026-03-11 22:50:47 +0530

    Add files via upload

---
+++ strlen.py
@@ -0,0 +1,10 @@
+# Program to find the length of a string
+
+# Taking input from the user
+text = input("Enter a string: ")
+
+# Finding the length of the string
+length = len(text)
+
+# Printing the result
+print("The length of the string is:", length)
