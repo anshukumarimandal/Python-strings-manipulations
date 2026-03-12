0f25884cc9bc676d9ae4c371d2c1c552a823c724
Anshukumari Mandal <anshukumarimandal09@gmail.com>
2026-03-12 20:03:53 +0530

    Add files via upload

---
+++ stringcat.py
@@ -0,0 +1,11 @@
+# Program for String Concatenation
+
+# Taking two strings from the user
+str1 = input("Enter first string: ")
+str2 = input("Enter second string: ")
+
+# Concatenating the strings
+result = str1 + " "+str2
+
+# Displaying the result
+print("Concatenated string is:", result)
