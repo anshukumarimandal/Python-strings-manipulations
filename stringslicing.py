b3abd8d614bb883ef0cff36849ec1c7f64ee8db8
Anshukumari Mandal <anshukumarimandal09@gmail.com>
2026-03-13 14:22:16 +0530

    Add files via upload

---
+++ stringslicing.py
@@ -0,0 +1,7 @@
+# Python program for string slicing
+text=str(input("Enter the string:"))
+print("Original String:", text)
+print("First 5 characters:", text[0:5])
+print("From index 6 to end:", text[6:])
+print("Last 3 characters:", text[-3:])
+print("Every second character:", text[::2])
