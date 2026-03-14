839b64f4d5c9076832c20e7da423fc7d9e59d138
Anshukumari Mandal <anshukumarimandal09@gmail.com>
2026-03-14 19:57:12 +0530

    Add files via upload

---
+++ stringcase.py
@@ -0,0 +1,9 @@
+# Program to convert string into upper and lower case
+
+text = str(input("Enter any string:"))
+
+# Convert to uppercase
+print("Uppercase:", text.upper())
+
+# Convert to lowercase
+print("Lowercase:", text.lower())
