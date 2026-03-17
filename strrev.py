c5f99548eda5c13ff913b57a980163f8530dd388
Anshukumari Mandal <anshukumarimandal09@gmail.com>
2026-03-17 21:36:19 +0530

    Add files via upload

---
+++ strrev.py
@@ -0,0 +1,10 @@
+# Program to reverse a string
+
+# Taking input from user
+string = input("Enter a string: ")
+
+# Reversing the string using slicing
+reversed_string = string[::-1]
+
+# Display the result
+print("Reversed string is:", reversed_string)
