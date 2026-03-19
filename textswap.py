02c8ac1c5a8c8214c6a96ba01dca1f03d9b85e95
Anshukumari Mandal <anshukumarimandal09@gmail.com>
2026-03-19 16:24:16 +0530

    Add files via upload

---
+++ textswap.py
@@ -0,0 +1,7 @@
+#write a program in python on reversing the case of each letter
+text = input("Enter a string: ")
+
+result = text.swapcase()
+
+print("Original string:", text)
+print("After swap case:", result)
